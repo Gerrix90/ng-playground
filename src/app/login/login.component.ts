4e9775f4971628b48de2f1fc5859d5f7315ce933
alvics <alvics71@hotmail.com>
2018-08-23 11:22:48 +1000

    adding register and login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,25 @@
+import { Router } from '@angular/router';
+import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(
+    private auth: AuthService,
+    private user: AppUser,
+    private router: Router
+  ) {}
 
-  constructor(private auth: AuthService) { }
-
-login() {
-  this.auth.login();
+  login() {
+    this.auth.login();
+  }
+  Register() {
+    this.router.navigateByUrl('/RegisterPage');
   }
 }
-
--- src/app/models/app-user.ts
+++ src/app/models/app-user.ts
@@ -1,5 +1,6 @@
 export interface AppUser {
   name: string;
   email: string;
+  password: string;
   isAdmin: boolean;
 }
